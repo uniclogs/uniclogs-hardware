f7359ffe999acb45029a2a0cfb7b98318bd4ccda
MaxMSchweitzer <mschwei2@pdx.edu>
2018-08-20 22:31:37 -0700

    Tabs to spaces because I'm a basic bitch

--- eb-ground-station/power_board/src/acs_statemachine-1.0/src/main.c
+++ eb-ground-station/power_board/src/acs_statemachine-1.0/src/main.c
@@ -1,12 +1,12 @@
 #include "acs_statemachine.h"
 
 int main(int argc, char *argv[]){
-	(void)argc;
-	(void)argv;
+    (void)argc;
+    (void)argv;
 
-	ACS acs = {0,0,0};
+    ACS acs = {0,0,0};
 
-	acs_statemachine(&acs);
+    acs_statemachine(&acs);
 
-	return 0;
+    return 0;
 }
